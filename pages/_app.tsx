b3a6f090e1977a03ef12db2dea48e3e8d2e20205
Aatif <email2aatif@gmail.com>
2023-04-26 23:38:38 -0500

    commit msgs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,11 @@ import '@/styles/style.css'
 import type { AppProps } from 'next/app'
 import "@aws-amplify/ui-react/styles.css";
 import config from '../src/aws-exports';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+
+
 import { Amplify, AuthModeStrategyType } from 'aws-amplify'
 import { AmplifyProvider } from '@aws-amplify/ui-react'
-import 'bootstrap/dist/css/bootstrap.css';
+
 import '@aws-amplify/ui-react/styles.css'
 Amplify.configure({
   ...config,
