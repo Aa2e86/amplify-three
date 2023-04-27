6e18f3db283b594f8f24d5f7daa9667668606da5
Aatif <email2aatif@gmail.com>
2023-04-27 00:00:58 -0500

    commit message

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,20 +2,15 @@ import '@/styles/globals.css'
 import '@/styles/style.css'
 import type { AppProps } from 'next/app'
 import "@aws-amplify/ui-react/styles.css";
-import config from '../src/aws-exports';
-
 
 import { Amplify, AuthModeStrategyType } from 'aws-amplify'
 import { AmplifyProvider } from '@aws-amplify/ui-react'
 
-import '@aws-amplify/ui-react/styles.css'
-Amplify.configure({
-  ...config,
-  DataStore: {
-    authModeStrategyType: AuthModeStrategyType.MULTI_AUTH
-  }
-})
 
+import "@aws-amplify/ui-react/styles.css";
+import awsconfig from '../src/aws-exports';
+
+Amplify.configure(awsconfig);
 
 
 function App({ Component, pageProps }: AppProps) {
