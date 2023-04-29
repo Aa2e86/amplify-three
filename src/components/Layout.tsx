8ce4230875b76e3600aa4e97a75021617dbc02a5
Aatif <email2aatif@gmail.com>
2023-04-28 21:25:57 -0500

    major format changes

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/router';
 import { Montserrat, Poppins } from 'next/font/google';
 import Script from 'next/script';
 import { Raleway } from 'next/font/google';
+import { Html } from 'next/document';
 function CustomFooter(){
     return(
       <div
@@ -115,25 +116,23 @@ function ProgressBar() {
     );
   };
 
-function gtag(){
+function Gtag(){
+  
   return(
     <>
-    <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-BBEKYRL9C2"/>
- <Script
-      id='google-analytics'
-      strategy="afterInteractive"
-      dangerouslySetInnerHTML={{
-        __html: `
-            window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+    <Script async strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-BBEKYRL9C2"/>
 
-  gtag('config', 'G-BBEKYRL9C2');{
-            page_path: window.location.pathname,
-          });
-        `,
-        }}
-/>
+<Script id = "google-analytics"
+strategy="afterInteractive"
+dangerouslySetInnerHTML={{__html:`
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-BBEKYRL9C2');
+
+`}}
+></Script>
 
     
     </>
@@ -147,11 +146,12 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <>
     <Head>
-      
+    
     <link href="https://fonts.googleapis.com/css2?family=Major+Mono+Display&family=Poppins:wght@300;400;500;600;700;800&family=Major+Mono+Display&family=Ubuntu:wght@400&display=swap&family=Sparten:wght@500&display=swap" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css?family=Raleway:wght@500&display=swap" rel="stylesheet" />
     <link rel="canonical" href={canonicalUrl} />
 </Head>
+<Gtag/>
       <div style={{ position: 'fixed', top: '0', width: '100%', height: '8px', zIndex: 999 }}>
       <NavBar
   height="70px"
