50b1fb9d767db53e60cca772cf0decabb50e5946
Aatif <email2aatif@gmail.com>
2023-04-29 19:53:42 -0500

    ssr test 1

--- pages/article/[articleTitle].js
+++ pages/article/[articleTitle].js
@@ -23,150 +23,7 @@ async function getArticleCoverImageUrl(key) {
 }
 
 
-  function ArticleData({ articleTitle } ) {
-    const [article, setArticle] = useState(null);
-    const [coverImageUrl, setCoverImageUrl] = useState(null);
-    const [modifiedText, setModifiedText] = useState(null);
-
-    useEffect(() => {
-      const fetchArticle = async () => {
-        try {
-          const [fetchedArticle] = await DataStore.query(Article, c => c.urltitle.eq(articleTitle));
-          setArticle(fetchedArticle);
-          
-          const url = await getArticleCoverImageUrl(fetchedArticle.coverimage);
-          setCoverImageUrl(url);
-          const modifiedText = await modifyText(fetchedArticle.text); 
-          setModifiedText(modifiedText);
-        } catch (error) {
-          console.log('Error fetching article', error);
-        }
-      };
-  
-      if (typeof window !== 'undefined') {
-        fetchArticle();
-      }
-    }, [articleTitle]);
-  
-  
-
-  async function modifyText(text) {
-    const regex = /<img[^>]+src="([^"]+)"[^>]*>/g;
-    const codeRegex = /<code\b[^>]*>(.*?)<\/code>/gs;
-    let newText = text;
-    let match = regex.exec(text);
-    while (match !== null) {
-      const url = match[1];
-      const newUrl = await getArticleCoverImageUrl(url);
-      const imgTag = match[0];
-      const newImgTag = imgTag.replace(url, newUrl).replace(">", ` style="display:block;box-shadow: 2px 4px 4px rgba(0, 0, 0, 0.3); border-radius:8px;width: 46vw; height: unset; margin: 0em auto;">`);
-      newText = newText.replace(imgTag, newImgTag);
-      match = regex.exec(text);
-    }
-    newText = newText.replace(codeRegex, '<code style="  border-radius:15px; background-color: #343541  ; color: #D1D5DB; padding: 0.5rem; display: inline-block;">$1</code>');
-  
-    // Add id attribute to each h2 element
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(text, 'text/html');
-    const headings = Array.from(doc.querySelectorAll('h2'));
-    headings.forEach((heading) => {
-      const id = heading.textContent.replace(/\s+/g, '-').toLowerCase();
  
-      heading.setAttribute('id', id);
-      newText = newText.replace(`<h2>${heading.textContent}</h2>`, `<h2 id="${id}">${heading.textContent}</h2>`);
-    });
-    
-    
-  
-    // Create table of contents
-    const toc = headings.map((heading) => {
-      const id = heading.getAttribute('id');
-      const text = heading.textContent;
-      return `<li><a href="#${id}">${text}</a></li>`;
-    }).join('');
-    newText = `<h3 style="margin-bottom:10px !important">Table of Contents</h3><ul style="margin-top:0rem !important;margin-bottom:-20px !important">${toc}</ul>` + newText;
-  // Get all links in the table of contents
-  
-  const tocDoc = parser.parseFromString(newText, 'text/html');
-  const tocLinks = Array.from(tocDoc.querySelectorAll('li a'));
- 
-  
-    return newText;
-  }
-  
-  
-  
-  
-
-  if (!article || !modifiedText) {
-    return null;
-  }
-
-  const { title, createdAt, description,author } = article;
-  const date = new Date(createdAt);
-  const options = { year: 'numeric', month: 'long', day:"numeric" };
-  const formattedDate = date.toLocaleDateString('en-US', options);
-  return (
-    <div  >
-      <Head>
-          <title>{title}</title>
-          <meta name="robots" content="index, follow"/>
-
-
-          <meta name="description" content={description} />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-   
-        </Head>
-   <div style={{height: "550px", backgroundColor:"#0F141D", display: "flex", flexDirection: "grid", alignItems: "center", justifyContent: "center",marginBottom:"-2%"}}>
-   <div style={{position:"relative",backgroundColor:"#0F141D", width:"40%", height:"100%", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
-    <h1 style={{color:"white", fontSize:"3.1vw", fontWeight:"700",padding:"30px", paddingLeft:"30px"}}>{title}</h1>
-    <h3 style={{ borderTop: "1px solid rgba(255, 255, 255, 0.5)",position: "absolute", bottom: 10, left: "50%", transform: "translateX(-50%)", color: "rgba(245, 245, 245, 0.9)",width:"60%", fontSize: "1rem", fontWeight: 500,padding:'2%', paddingBottom: "3%", textAlign: "center" }}>
-  {author} at {formattedDate}   
-  </h3>
-    {/* <h2 style={{position:"absolute",color:"white", fontSize:"1em", fontWeight:"300", bottom:"40%",left:"10%"}}>{author}</h2> */}
-  </div>
-  <div style={{backgroundImage:`url(${coverImageUrl})`, backgroundSize:"cover", width:"60%",height:"100%"}}>
-  </div>
-</div>
-
-
-
-
-      
-      
-
-      
-      <ArticleUI 
-        overrides={{
-          Title: { children: title,fontSize:"1.5vw",fontWeight:"500",lineHeight:"1.2" },
-          Date: { children: "posted on "+ new Date(createdAt).toLocaleDateString(),fontSize:"0.8rem"},
-          Description: { children: description,fontSize:"1rem",fontWeight:"400", },
-          Content: { 
-            className:"articleContent",
-            children: null,
-            dangerouslySetInnerHTML: { __html: modifiedText },
-            fontSize: "1.2rem",
-            paddingLeft:"10%",
-            paddingRight:"5%",
-            
-            fontWeight: "200",
-            lineHeight:"1.6srem",
-            backgroundColor:"#ffffff",
-            borderRadius:"15px",
-            minWidth:"70%",
-            alignSelf:"center",
-            margingRight:"2% !important",
-            
-          },
-          Coverimage:{src: coverImageUrl, height:" 22rem"},
-          style:{backgroundColor:"#CED8E6"}
-        }}
-      />
-      
-    </div>
-    
-  );
-}
 function CustomFooter(){
   return(
     <div
@@ -316,32 +173,176 @@ function ProgressBar() {
   );
 };
 
-export async function getServerSideProps(context) {
-  const { params } = context;
-  const { articleTitle } = params; // replace with your own parameter name
 
-  const SSRContext = withSSRContext();
-  const [fetchedArticle] = await SSRContext.DataStore.query(Article, c => c.urltitle.eq(articleTitle));
-  
+
+
+
+export async function getStaticPaths({ req }) {
+  const SSR = withSSRContext({ req });
+  const darticles = await SSR.DataStore.query(Article);
   return {
-    props: {
-      article: JSON.parse(JSON.stringify(fetchedArticle)),
-    },
+    paths: darticles.map(darticle => ({ params: { articleTitle:darticle.title }})),
+    fallback: true
   };
 }
 
 
-
-export default function ArticlePage() {
-  const router = useRouter();
-  const { articleTitle } = router.query;
-  const name = typeof articleTitle === 'string' ? articleTitle : '';
+export async function getStaticProps(context) {
+  const SSR = withSSRContext({ context });
+  const removedash = typeof  context.params.articleTitle === 'string' ?  context.params.articleTitle : '';
   
+  const dartt = await SSR.DataStore.query(Article, c => c.urltitle.eq(removedash));
+  const { title, createdAt, description,author,coverimage,text } =  serializeModel(dartt)[0]
+
+  return {
+    props: { title, createdAt, description,author,coverimage,text },
+  }
+}
+
+
+function ArticleData({dart} ) {
+  const fetchedArticle= dart
+  const [article, setArticle] = useState(null);
+  const [coverImageUrl, setCoverImageUrl] = useState(null);
+  const [modifiedText, setModifiedText] = useState(null);
+
+
+  useEffect(() => {
+    const fetchArticle = async () => {
+      try {
+      
+        setArticle(fetchedArticle);
+        
+        const url = await getArticleCoverImageUrl(fetchedArticle.coverimage);
+        setCoverImageUrl(url);
+        const modifiedText = await modifyText(fetchedArticle.text); 
+        setModifiedText(modifiedText);
+      } catch (error) {
+        console.log('Error fetching article', error);
+      }
+    };
+
+    if (typeof window !== 'undefined') {
+      fetchArticle();
+    }
+  });
+
+
+
+async function modifyText(text) {
+  const regex = /<img[^>]+src="([^"]+)"[^>]*>/g;
+  const codeRegex = /<code\b[^>]*>(.*?)<\/code>/gs;
+  let newText = text;
+  let match = regex.exec(text);
+  while (match !== null) {
+    const url = match[1];
+    const newUrl = await getArticleCoverImageUrl(url);
+    const imgTag = match[0];
+    const newImgTag = imgTag.replace(url, newUrl).replace(">", ` style="display:block;box-shadow: 2px 4px 4px rgba(0, 0, 0, 0.3); border-radius:8px;width: 46vw; height: unset; margin: 0em auto;">`);
+    newText = newText.replace(imgTag, newImgTag);
+    match = regex.exec(text);
+  }
+  // newText = newText.replace(codeRegex, '<code style="  border-radius:15px; background-color: #343541  ; color: #D1D5DB; padding: 0.5rem; display: inline-block;">$1</code>');
+
+  // Add id attribute to each h2 element
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(text, 'text/html');
+  const headings = Array.from(doc.querySelectorAll('h2'));
+  headings.forEach((heading) => {
+    const id = heading.textContent.replace(/\s+/g, '-').toLowerCase();
+
+    heading.setAttribute('id', id);
+    newText = newText.replace(`<h2>${heading.textContent}</h2>`, `<h2 id="${id}">${heading.textContent}</h2>`);
+  });
+  
+  
+
+  // Create table of contents
+  const toc = headings.map((heading) => {
+    const id = heading.getAttribute('id');
+    const text = heading.textContent;
+    return `<li><a href="#${id}">${text}</a></li>`;
+  }).join('');
+  newText = `<h3 style="margin-bottom:10px !important">Table of Contents</h3><ul style="margin-top:0rem !important;margin-bottom:-20px !important">${toc}</ul>` + newText;
+  // Get all links in the table of contents
+
+  const tocDoc = parser.parseFromString(newText, 'text/html');
+  const tocLinks = Array.from(tocDoc.querySelectorAll('li a'));
+
+
+    return newText;
+  }
+ 
+  const { title, createdAt, description,author } = fetchedArticle;
+  const date = new Date(createdAt);
+  const options = { year: 'numeric', month: 'long', day:"numeric" };
+  const formattedDate = date.toLocaleDateString('en-US', options);
+  return (
+    <div  >
+      <Head>
+          <title>{title}</title>
+          <meta name="robots" content="index, follow"/>
+
+
+          <meta name="description" content={description} />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+   
+        </Head>
+   <div style={{height: "550px", backgroundColor:"#0F141D", display: "flex", flexDirection: "grid", alignItems: "center", justifyContent: "center",marginBottom:"-2%"}}>
+   <div style={{position:"relative",backgroundColor:"#0F141D", width:"40%", height:"100%", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
+    <h1 style={{color:"white", fontSize:"3.1vw", fontWeight:"700",padding:"30px", paddingLeft:"30px"}}>{title}</h1>
+    <h3 style={{ borderTop: "1px solid rgba(255, 255, 255, 0.5)",position: "absolute", bottom: 10, left: "50%", transform: "translateX(-50%)", color: "rgba(245, 245, 245, 0.9)",width:"60%", fontSize: "1rem", fontWeight: 500,padding:'2%', paddingBottom: "3%", textAlign: "center" }}>
+  {author} at {formattedDate}   
+  </h3>
+    {/* <h2 style={{position:"absolute",color:"white", fontSize:"1em", fontWeight:"300", bottom:"40%",left:"10%"}}>{author}</h2> */}
+  </div>
+  <div style={{backgroundImage:`url(${coverImageUrl})`, backgroundSize:"cover", width:"60%",height:"100%"}}>
+  </div>
+</div>
+<ArticleUI 
+        overrides={{
+          Title: { children: title,fontSize:"1.5vw",fontWeight:"500",lineHeight:"1.2" },
+          Date: { children: "posted on "+ new Date(createdAt).toLocaleDateString(),fontSize:"0.8rem"},
+          Description: { children: description,fontSize:"1rem",fontWeight:"400", },
+          Content: { 
+            className:"articleContent",
+            children: null,
+            dangerouslySetInnerHTML: { __html: modifiedText },
+            fontSize: "1.2rem",
+            paddingLeft:"10%",
+            paddingRight:"5%",
+            
+            fontWeight: "200",
+            lineHeight:"1.6srem",
+            backgroundColor:"#ffffff",
+            borderRadius:"15px",
+            minWidth:"70%",
+            alignSelf:"center",
+            margingRight:"2% !important",
+            
+          },
+          Coverimage:{src: coverImageUrl, height:" 22rem"},
+          style:{backgroundColor:"#CED8E6"}
+        }}
+      />
+</div>
+)
+
+}
+
+
+
+
+
+export default function ArticlePage(props) {
+
   return (
     <>
-    <Layout>
-      <ArticleData articleTitle={name} />
+    
+     
 
+    <Layout>
+    <ArticleData dart={props}  />
       <div style={{ marginTop: "1em", marginLeft: "8em", marginRight: "9em" }}>
       <StoryCollectionData/>
       </div>
